6b3db0313164d6035b697372f9cd9952d00bdeca
Oscar Frischmann <oscarfrisc@gmail.com>
2023-09-02 18:45:35 -0300

    main courses cards

--- js/assets/courses.js
+++ js/assets/courses.js
@@ -10,8 +10,8 @@ class Course {
 
 const courses = [];
 
-courses.push(new Course('cursos', 'de inglés general', './img/colectivo.amarillo.png', 'Están divididos en niveles desde Elemental hasta Avanzado, homologados con el MCER. Cada nivel se completa al finalizar dos cuatrimestres de estudio.', 'ver más'));
-courses.push(new Course('cambbridge', 'preparación de exámenes', './img/cambridge.png', 'En estos cursos brindamos todas las herramientas necesarias para poder rendir los exámenes internacionales de Cambridge, FIRST y AVANCED. La preparación se completa al cabo de dos cuatrimestres de estudio. Es necesaria una evaluación de nivel previa.', 'ver más'));
+courses.push(new Course('cursos', 'de inglés general', './img/colectivo-amarillo.png', 'Están divididos en niveles desde Elemental hasta Avanzado, homologados con el MCER. Cada nivel se completa al finalizar dos cuatrimestres de estudio.', 'ver más'));
+courses.push(new Course('cambridge', 'preparación de exámenes', './img/cambridge.png', 'En estos cursos brindamos todas las herramientas necesarias para poder rendir los exámenes internacionales de Cambridge, FIRST y AVANCED. La preparación se completa al cabo de dos cuatrimestres de estudio. Es necesaria una evaluación de nivel previa.', 'ver más'));
 courses.push(new Course('talleres', 'de conversación', './img/talking-color.png', 'Si estás buscando hablar con más fluidez, incorporar más vocabulario y poder practicar conversación en grupos reducidos, este espacio es para vos. A partir de nivel pre-intermedio hasta avanzado.', 'ver más'));
 courses.push(new Course('seminarios', 'temáticos', './img/four-faces-color.png', 'Si tenés un nivel de inglés intermedio o superior, podés participar de nuestros seminarios temáticos donde vas a poder usar el idioma para aprender sobre distintos temas. Algunas de nuestras propuestas son: inglés para viajes, inglés para el trabajo, astrología en inglés y amantes del cine.', 'ver más'));
 courses.push(new Course('clases', 'individuales', './img/laptop-verde.png', '¿Necesitás acomodar las clases a tus horarios? ¿Necesitás inglés con fines específicos? ¡Este es el espacio para vos!', 'ver más'));
@@ -33,21 +33,40 @@ courses.forEach((course, i) => {
     card.appendChild(headerDiv);
     headerDiv.setAttribute('id', `div${i}`)
     headerDiv.classList.add('courses__card-header');
-
     tag.forEach((tag, tagI) => {
-        if (tag === 'h4' || tag === 'img' || tag === 'h5') {
-            for (const el in course) {
-                console.log(el)
-            }
+        if (tag === 'h4') {
             const htmlTag = document.createElement(tag);
+            htmlTag.innerText = course.tittleOne;
             headerDiv.appendChild(htmlTag);
-            console.log(course);
-        } else {
+        } else if (tag === 'img') {
+            const htmlTag = document.createElement(tag);
+            headerDiv.appendChild(htmlTag)
+            htmlTag.src = course.img;
+        } else if (tag === 'h5') {
+            const htmlTag = document.createElement(tag);
+            headerDiv.appendChild(htmlTag)
+            htmlTag.innerText = course.tittleTwo;
+        } else if (tag === 'p') {
             const htmlTag = document.createElement(tag);
             card.appendChild(htmlTag)
+            htmlTag.innerText = course.paragraph;
+        }else {
+            const htmlTag = document.createElement(tag);
+            card.appendChild(htmlTag)
+            htmlTag.innerText = course.btnText;
+            htmlTag.classList.add('btn')
+            htmlTag.style.marginLeft
         }
     })
 })
+
+const cardHeadersBG = document.getElementsByClassName('courses__card-container');
+
+cardHeadersBG[0].style.backgroundImage = 'linear-gradient(180deg, #40B9B9 0% 30%, transparent 30% 100% )'
+cardHeadersBG[1].style.backgroundImage = 'linear-gradient(180deg, #FBE800 0% 30%, transparent 30% 100% )'
+cardHeadersBG[2].style.backgroundImage = 'linear-gradient(180deg, #E2318A 0% 30%, transparent 30% 100% )'
+cardHeadersBG[3].style.backgroundImage = 'linear-gradient(180deg, #40B9B9 0% 30%, transparent 30% 100% )'
+cardHeadersBG[4].style.backgroundImage = 'linear-gradient(180deg, #FBE800 0% 30%, transparent 30% 100% )'
 
 
 

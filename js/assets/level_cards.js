9e9e0bc9db035c31e73bea530a9f08ef2bf4d8eb
Oscar Frischmann <oscarfrisc@gmail.com>
2023-09-06 15:56:07 -0300

    prices cards complete

--- js/assets/level_cards.js
+++ js/assets/level_cards.js
@@ -8,7 +8,7 @@ class Level {
         this.tittle = tittle;
         this.level = level;
         this.names = names;
-        this.description = description; //array of strings
+        this.description = description;
     }
 }
 
@@ -77,39 +77,95 @@ coursesArr.forEach((e, i) => {
     cardButton.setAttribute('id', `courseBtn${i}`);
 });
 
-const btn0 = document.getElementById('courseBtn0')
-btn0.addEventListener('click', () => {
-    const card0 = document.getElementsByClassName('card0');
-    card0[0].classList.add('opacity1');
-    setTimeout(() => card0[0].classList.add('display-none'), 1000);
-    const descriprionCardA = document.getElementById('descriptionCardA')
-    setTimeout(() => {
-        descriprionCardA.classList.remove('display-none');
-        descriprionCardA.classList.add('opacity0');
+// btn0.addEventListener('click', () => {
+//     const card0 = document.getElementsByClassName('card0');
+//     card0[0].classList.add('opacity1');
+//     setTimeout(() => card0[0].classList.add('display-none'), 1000);
+//     const descriprionCardA = document.getElementById('descriptionCardA')
+//     setTimeout(() => {
+//         descriprionCardA.classList.remove('display-none');
+//         descriprionCardA.classList.add('opacity0');
 
 
-    }, 1000);
-})
-
+//     }, 1000);
+// })
+const btn0 = document.getElementById('courseBtn0');
 const btn1 = document.getElementById('courseBtn1');
 const btn2 = document.getElementById('courseBtn2');
 
 function showCourseDescription(index, descriptionCard) {
-    
-        const card0 = document.getElementsByClassName(`card${index}`);
-        card0[0].classList.add('opacity1');
-        setTimeout(() => card0[0].classList.add('display-none'), 1000);
-        const descriprionCardA = document.getElementById(descriptionCard)
-        setTimeout(() => {
-            descriprionCardA.classList.remove('display-none');
-            descriprionCardA.classList.add('opacity0');
-        }, 1000);
+    const card0 = document.getElementsByClassName(`card${index}`);
+    card0[0].classList.add('opacity1');
+    setTimeout(() => card0[0].classList.add('display-none'), 1000);
+    const descriprionCardA = document.getElementById(descriptionCard)
+    setTimeout(() => {
+        descriprionCardA.classList.remove('display-none');
+        descriprionCardA.classList.add('opacity0');
+
+    }, 1000);
 }
-btn1.addEventListener('click', ()=> {
 
-    showCourseDescription('1', 'descriptionCardB')
-})
-btn2.addEventListener('click', ()=> {
+btn0.addEventListener('click', () => showCourseDescription('0', 'descriptionCardA'));
+btn1.addEventListener('click', () => showCourseDescription('1', 'descriptionCardB'));
+btn2.addEventListener('click', () => showCourseDescription('2', 'descriptionCardC'));
 
-    showCourseDescription('2', 'descriptionCardC')
-})
+const showPriceBtn = document.querySelectorAll('.courses__description--card .btn');
+const hideDescriptionCard = document.querySelectorAll('.courses__description--card');
+
+const short = '1.15';
+const long = '1.30';
+let monthPriceShort = '8000';
+let monthPriceLong = '9500';
+
+showPriceBtn.forEach((e, i) => {
+    showPriceBtn[i].addEventListener('click', () => {
+        monthPriceShort = '8000';
+        monthPriceLong = '9500';
+        hideDescriptionCard[i].innerHTML = `
+        <div class="opacity0 courses__description--price">
+            <h2>Cursos<br><span>de inglés general</span></h2>
+            <img src="../img/colectivo-amarillo.png">
+            <p class="paragraph" id="paragraph${i}">Todos los cursos de Inglés general duran 4 meses con los siguientes precios por mes expresados en pesos argentinos.</p>
+            <div class="price-container">
+                <p id="short${i}"><span>${short}hs </span>por semana: <span>${monthPriceShort}</span></p>
+                <button id="showDiscountShort${i}">%</button>
+                </div>
+                <div class="price-container">
+                <p id="long${i}"><span>${long}</span>hs por semana: <span>${monthPriceLong}</span></p>
+                <button id="showDiscountLong${i}">%</button>
+            </div>
+            <div class="btn-container">
+                <button class="btn">Cerrar</button>
+                <button class="btn">Escribinos</button>
+            </div>
+        </div>
+            `;
+        console.log(i);
+        const paragraph = document.getElementById(`paragraph${i}`);
+        const shortCourse = document.getElementById(`short${i}`);
+        const longCourse = document.getElementById(`long${i}`);
+        const showDiscountLong = document.getElementById(`showDiscountLong${i}`);
+        const showDiscountShort = document.getElementById(`showDiscountShort${i}`);
+
+        function changePrices() {
+            let monthPriceShort = '31000';
+            let monthPriceLong = '37000';
+            paragraph.textContent = 'Este precio es para los que pagan el cuatrimestre completo.';
+            shortCourse.innerHTML = `<span>${short}</span>hs por semana: <span>${monthPriceShort}</span>`;
+            longCourse.innerHTML = `<span>${long}</span>hs por semana: <span>${monthPriceLong}</span>`;
+        }
+        showDiscountShort.addEventListener('click', () => {
+            changePrices();
+        })
+        showDiscountLong.addEventListener('click', () => {
+            changePrices();
+        })
+    });
+});
+
+
+
+
+
+// console.log(showDiscount)
+// console.log(showDiscount2)

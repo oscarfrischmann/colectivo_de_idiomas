9887b0faa0d51ac73057e0b2b3440ae26ff8c051
Oscar Frischmann <oscarfrisc@gmail.com>
2024-06-18 12:25:53 -0300

    cambridge firebase but prices

--- js/assets/cambridge.js
+++ js/assets/cambridge.js
@@ -1,94 +1,130 @@
-const cambCourses = document.getElementsByClassName('cambridge__courses');
-const cambUL = document.querySelectorAll('.cambridge__card ul');
-// const cambButtons = document.getElementsByClassName('cambridge__buttons');
-const b2PriceBtn = document.getElementById('b2PriceBtn');
-const c1PriceBtn = document.getElementById('c1PriceBtn');
-const cambWasap = document.getElementsByClassName('cambWasap');
-for (button of cambWasap) {
-	button.addEventListener('click', () => openWasap());
-}
-console.log(document.querySelector('.cambridge__card h2').innerHTML);
+import * as login from "./login.js";
 
-const priceBtn = document.querySelectorAll('.btn.price');
-for (let i = 0; i < priceBtn.length; i++) {
-	priceBtn[i].addEventListener('click', () => {
-		console.log(i);
-		priceBtn[i].textContent = 'home';
-		priceBtn[i].setAttribute('id', `changed${i}`);
-		priceBtn[i].classList.remove('price');
-		const changed = document.getElementById(`changed${i}`);
-		console.log(changed);
-		changed.addEventListener('click', () => {
-			window.location = '../index.html';
-		});
-		cambUL[i].classList.add('opacity1');
-		setTimeout(() => cambUL[i].classList.add('display-none'), 1000);
-		setTimeout(() => {
-			const cambPrice = document.createElement('div');
-			cambPrice.classList.add('price');
-			if (document.querySelector('.cambridge__card h2').innerHTML === 'B2 First<br> C1 Advanced') {
-				console.log('WE ARE INNNN');
-				cambPrice.innerHTML = `
+let monthlyCamPay = 14000;
+let courseCambPay = 120000;
+
+const priceCamb = document.getElementById("pricesCamb");
+priceCamb.innerHTML = `
+<div class="courses__description--price">
+<h2>Precios</h2>
+	
 	<div class="price-container">
-			<p id="longB2"><span>1.30</span>hs por semana:</p>
-			<div class="div">
-				<h3>Pago Mensual</h3>
-				<span class="span${i}">${cambCoursePrice}</span>
-			</div>
-			<div class="div">
-				<h3>Pago Curso Completo</h3>
-				<span class="span${i}">${cambCoursePriceFull}</span>
-			</div>
-			</div>
-
-`;
-			} else {
-				cambPrice.innerHTML = `
+		<p id="long0"><span>Pago mensual: <span>$ ${monthlyCamPay}</span></p>
+	</div>
 	<div class="price-container">
-			<p id="longB2"><span>1.30</span>hs por semana:</p>
-			<div class="div">
-				<h3>Pago Mensual</h3>
-				<span class="span${i}">${conversationPrice}</span>
-			</div>
-			<div class="div">
-				<h3>Pago Curso Completo</h3>
-				<span class="span${i}">${conversationPriceFull}</span>
-			</div>
-			</div>
+		<p id="long0"><span>Pago curso completo:</span> <span>$ ${courseCambPay}</span></p>
+	</div>
+	
+	<div class="btn-container">
+	
+		<button class="btn" onclick="openWasap()">Escribinos</button>
+	</div>
+	<span class="courses__description--test">Escribinos para solicitar un test de nivel!</span>
+</div>
+	`;
+console.log(login.cambScheduleDB2);
+const schedule = document.getElementById("scheduleCamb");
+login.cambScheduleDB2.cambSchedule.forEach((courses, i) => {
+  schedule.innerHTML += `
+	<div class="price-container">
+	<span class="sc1">${courses.name} </span>
+	<span class="sc2">${courses.date} </span>
+	<ruby class="sc3">${courses.time}<rt>${courses.day}</rt> </ruby>
+	</div>
+	`;
+});
 
-`;
-			}
+// const cambCourses = document.getElementsByClassName('cambridge__courses');
+// const cambUL = document.querySelectorAll('.cambridge__card ul');
+// // const cambButtons = document.getElementsByClassName('cambridge__buttons');
+// const b2PriceBtn = document.getElementById('b2PriceBtn');
+// const c1PriceBtn = document.getElementById('c1PriceBtn');
+// const cambWasap = document.getElementsByClassName('cambWasap');
+// for (button of cambWasap) {
+// 	button.addEventListener('click', () => openWasap());
+// }
+// console.log(document.querySelector('.cambridge__card h2').innerHTML);
 
-			cambUL[i].insertAdjacentElement('beforebegin', cambPrice);
-			const usd = document.getElementsByClassName(`span${i}`);
-			console.log(usd);
-			checkbox.addEventListener('click', () => {
-				if (checkbox.checked) {
-					for (price of usd) {
-						const usdPrice = document.getElementsByClassName(`span${i}`);
+// const priceBtn = document.querySelectorAll('.btn.price');
+// for (let i = 0; i < priceBtn.length; i++) {
+// 	priceBtn[i].addEventListener('click', () => {
+// 		console.log(i);
+// 		priceBtn[i].textContent = 'home';
+// 		priceBtn[i].setAttribute('id', `changed${i}`);
+// 		priceBtn[i].classList.remove('price');
+// 		const changed = document.getElementById(`changed${i}`);
+// 		console.log(changed);
+// 		changed.addEventListener('click', () => {
+// 			window.location = '../index.html';
+// 		});
+// 		cambUL[i].classList.add('opacity1');
+// 		setTimeout(() => cambUL[i].classList.add('display-none'), 1000);
+// 		setTimeout(() => {
+// 			const cambPrice = document.createElement('div');
+// 			cambPrice.classList.add('price');
+// 			if (document.querySelector('.cambridge__card h2').innerHTML === 'B2 First<br> C1 Advanced') {
+// 				console.log('WE ARE INNNN');
+// 				cambPrice.innerHTML = `
+// 	<div class="price-container">
+// 			<p id="longB2"><span>1.30</span>hs por semana:</p>
+// 			<div class="div">
+// 				<h3>Pago Mensual</h3>
+// 				<span class="span${i}">${cambCoursePrice}</span>
+// 			</div>
+// 			<div class="div">
+// 				<h3>Pago Curso Completo</h3>
+// 				<span class="span${i}">${cambCoursePriceFull}</span>
+// 			</div>
+// 			</div>
 
-						if (document.querySelector('.cambridge__card h2').textContent === 'B2 First') {
-							usdPrice[0].textContent = cambCoursePriceUSD;
-							usdPrice[1].textContent = cambCoursePriceUSDFull;
-						} else {
-							usdPrice[0].textContent = conversationPriceUSD;
-							usdPrice[1].textContent = conversationPriceUSDFull;
-						}
-					}
-				} else {
-					for (price of usd) {
-						const usdPrice = document.getElementsByClassName(`span${i}`);
+// `;
+// 			} else {
+// 				cambPrice.innerHTML = `
+// 	<div class="price-container">
+// 			<p id="longB2"><span>1.30</span>hs por semana:</p>
+// 			<div class="div">
+// 				<h3>Pago Mensual</h3>
+// 				<span class="span${i}">${conversationPrice}</span>
+// 			</div>
+// 			<div class="div">
+// 				<h3>Pago Curso Completo</h3>
+// 				<span class="span${i}">${conversationPriceFull}</span>
+// 			</div>
+// 			</div>
 
-						if (document.querySelector('.cambridge__card h2').textContent === 'B2 First') {
-							usdPrice[0].textContent = cambCoursePrice;
-							usdPrice[1].textContent = cambCoursePriceFull;
-						} else {
-							usdPrice[0].textContent = conversationPrice;
-							usdPrice[1].textContent = conversationPriceFull;
-						}
-					}
-				}
-			});
-		}, 999);
-	});
-}
+// `;
+// 			}
+
+// 			cambUL[i].insertAdjacentElement('beforebegin', cambPrice);
+// 			const usd = document.getElementsByClassName(`span${i}`);
+// 			console.log(usd);
+// 			checkbox.addEventListener('click', () => {
+// 				if (checkbox.checked) {
+// 					for (price of usd) {
+// 						const usdPrice = document.getElementsByClassName(`span${i}`);
+
+// 						if (document.querySelector('.cambridge__card h2').textContent === 'B2 First') {
+// 							usdPrice[0].textContent = cambCoursePriceUSD;
+// 							usdPrice[1].textContent = cambCoursePriceUSDFull;
+// 						} else {
+// 							usdPrice[0].textContent = conversationPriceUSD;
+// 							usdPrice[1].textContent = conversationPriceUSDFull;
+// 						}
+// 					}
+// 				} else {
+// 					for (price of usd) {
+// 						const usdPrice = document.getElementsByClassName(`span${i}`);
+
+// 						if (document.querySelector('.cambridge__card h2').textContent === 'B2 First') {
+// 							usdPrice[0].textContent = cambCoursePrice;
+// 							usdPrice[1].textContent = cambCoursePriceFull;
+// 						} else {
+// 							usdPrice[0].textContent = conversationPrice;
+// 							usdPrice[1].textContent = conversationPriceFull;
+// 						}
+// 					}
+// 				}
+// 			});
+// 		}, 999);
+// 	});
+// }
--- js/assets/login.js
+++ js/assets/login.js
@@ -1,165 +1,247 @@
-import sweetalert2 from 'https://cdn.jsdelivr.net/npm/sweetalert2@11.11.1/+esm';
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js';
+import sweetalert2 from "https://cdn.jsdelivr.net/npm/sweetalert2@11.11.1/+esm";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import {
-	getFirestore,
-	collection,
-	getDocs,
-	getDoc,
-	doc,
-	setDoc,
-	deleteDoc,
-} from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js';
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  setDoc,
+  deleteDoc,
+} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 const firebaseConfig = {
-	apiKey: 'AIzaSyAtQccyF0CBM-f9kRhY15B4E7tCpqLCmDs',
-	authDomain: 'colectivo-de-idiomas.firebaseapp.com',
-	projectId: 'colectivo-de-idiomas',
-	storageBucket: 'colectivo-de-idiomas.appspot.com',
-	messagingSenderId: '21235746434',
-	appId: '1:21235746434:web:54c3b1e041f3fb0d94169b',
+  apiKey: "AIzaSyAtQccyF0CBM-f9kRhY15B4E7tCpqLCmDs",
+  authDomain: "colectivo-de-idiomas.firebaseapp.com",
+  projectId: "colectivo-de-idiomas",
+  storageBucket: "colectivo-de-idiomas.appspot.com",
+  messagingSenderId: "21235746434",
+  appId: "1:21235746434:web:54c3b1e041f3fb0d94169b",
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const pricesFormDB = document.getElementById('pricesFormDB');
-
+//*Precios de Cursos Generales
+const pricesFormDB = document.getElementById("pricesFormDB");
 async function publishGeneralCoursesPricesDB(e) {
-	e.preventDefault();
-	try {
-		const prices = {
-			monthly: Number(pricesFormDB['monthlyPay'].value),
-			course: Number(pricesFormDB['coursePay'].value),
-		};
-		await setDoc(doc(db, 'prices', 'generalCourses'), prices);
-		await sweetalert2.fire({
-			title: 'Precios de Cursos generales PUBLICADOS!',
-			background: '#93E9BE',
-			toast: true,
-			position: 'top-end',
-			showConfirmButton: false,
-			width: 'fit-content',
-			timer: 3000,
-			timerProgressBar: true,
-		});
-		return prices;
-	} catch (err) {
-		sweetalert2.fire({
-			title: 'ERROR!',
-			background: ' #DB231C',
-			toast: true,
-			position: 'top-end',
-			showConfirmButton: false,
-			width: 'fit-content',
-			timer: 3000,
-			timerProgressBar: true,
-		});
-		throw new Error('puclishCoursePricesDB', err);
-	}
+  e.preventDefault();
+  try {
+    const prices = {
+      monthly: Number(pricesFormDB["monthlyPay"].value),
+      course: Number(pricesFormDB["coursePay"].value),
+    };
+    await setDoc(doc(db, "prices", "generalCourses"), prices);
+    await sweetalert2.fire({
+      title: "Precios de Cursos generales PUBLICADOS!",
+      background: "#93E9BE",
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      width: "fit-content",
+      timer: 3000,
+      timerProgressBar: true,
+    });
+    return prices;
+  } catch (err) {
+    sweetalert2.fire({
+      title: "ERROR!",
+      background: " #DB231C",
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      width: "fit-content",
+      timer: 3000,
+      timerProgressBar: true,
+    });
+    throw new Error("puclishCoursePricesDB", err);
+  }
 }
-pricesFormDB.addEventListener('submit', await publishGeneralCoursesPricesDB);
+pricesFormDB.addEventListener("submit", await publishGeneralCoursesPricesDB);
 
 async function getPrices() {
-	try {
-		const pricesCol = doc(db, 'prices', 'generalCourses');
-		const pricesSnapshot = await getDoc(pricesCol);
-		const pricesList = pricesSnapshot.data();
-		return pricesList;
-	} catch (err) {
-		throw new Error('get Docs', err);
-	}
+  try {
+    const pricesCol = doc(db, "prices", "generalCourses");
+    const pricesSnapshot = await getDoc(pricesCol);
+    const pricesList = pricesSnapshot.data();
+    return pricesList;
+  } catch (err) {
+    throw new Error("get Docs", err);
+  }
 }
 
 export const pricesDB = await getPrices();
 
+//*Test agregar cursos generales
 const coursesScheduleDataDB2 = { coursesSchedule: [] };
-const coursesForm = document.getElementById('coursesScheduleDB');
-const addButton = document.getElementById('add');
-addButton.addEventListener('click', (e) => {
-	e.preventDefault();
-	const course = {
-		name: coursesForm['name'].value,
-		date: coursesForm['date'].value,
-		day: coursesForm['day'].value,
-		time: coursesForm['time'].value,
-	};
-	coursesScheduleDataDB2.coursesSchedule.push(course);
-	console.log(coursesScheduleDataDB2);
-	const showCourses = document.createElement('div');
-	coursesForm.insertAdjacentElement('afterend', showCourses);
-	coursesScheduleDataDB2.coursesSchedule.forEach((course) => {
-		showCourses.innerHTML = `
+const coursesForm = document.getElementById("coursesScheduleDB");
+const addButton = document.getElementById("add");
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const course = {
+    name: coursesForm["name"].value,
+    date: coursesForm["date"].value,
+    day: coursesForm["day"].value,
+    time: coursesForm["time"].value,
+  };
+  coursesScheduleDataDB2.coursesSchedule.push(course);
+  console.log(coursesScheduleDataDB2);
+  const showCourses = document.createElement("div");
+  coursesForm.insertAdjacentElement("afterend", showCourses);
+  coursesScheduleDataDB2.coursesSchedule.forEach((course) => {
+    showCourses.innerHTML = `
   ${course.name} | ${course.date} | ${course.day} | ${course.time}
     `;
-		console.log(course);
-	});
+    console.log(course);
+  });
 });
-const publishSchedButton = document.getElementById('publishSchedButton');
-publishSchedButton.addEventListener('click', async (e) => {
-	e.preventDefault();
-	try {
-		await setDoc(doc(db, 'coursesSchedule', 'coursesSchedule'), coursesScheduleDataDB2);
-		await sweetalert2.fire({
-			title: ' Cursos generales PUBLICADOS!',
-			background: '#93E9BE',
-			toast: true,
-			position: 'top-end',
-			showConfirmButton: false,
-			width: 'fit-content',
-			timer: 3000,
-			timerProgressBar: true,
-		});
-	} catch (err) {
-		sweetalert2.fire({
-			title: 'ERROR!',
-			background: ' #DB231C',
-			toast: true,
-			position: 'top-end',
-			showConfirmButton: false,
-			width: 'fit-content',
-			timer: 3000,
-			timerProgressBar: true,
-		});
-		throw new Error('publish courses', err);
-	}
+//*publicar Cursos Generales
+const publishSchedButton = document.getElementById("publishSchedButton");
+publishSchedButton.addEventListener("click", async (e) => {
+  e.preventDefault();
+  try {
+    await setDoc(
+      doc(db, "coursesSchedule", "coursesSchedule"),
+      coursesScheduleDataDB2
+    );
+    await sweetalert2.fire({
+      title: " Cursos generales PUBLICADOS!",
+      background: "#93E9BE",
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      width: "fit-content",
+      timer: 3000,
+      timerProgressBar: true,
+    });
+  } catch (err) {
+    sweetalert2.fire({
+      title: "ERROR!",
+      background: " #DB231C",
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      width: "fit-content",
+      timer: 3000,
+      timerProgressBar: true,
+    });
+    throw new Error("publish courses", err);
+  }
 });
 
 async function getCoursesSchedule() {
-	try {
-		const courseSshedulesCol = doc(db, 'coursesSchedule', 'coursesSchedule');
-		const courseSshedulesSnapshot = await getDoc(courseSshedulesCol);
-		const courseSshedulesList = courseSshedulesSnapshot.data();
-		return courseSshedulesList;
-	} catch (err) {
-		throw new Error('get CoursesShedule', err);
-	}
+  try {
+    const courseSshedulesCol = doc(db, "coursesSchedule", "coursesSchedule");
+    const courseSshedulesSnapshot = await getDoc(courseSshedulesCol);
+    const courseSshedulesList = courseSshedulesSnapshot.data();
+    return courseSshedulesList;
+  } catch (err) {
+    throw new Error("get CoursesShedule", err);
+  }
 }
 
 export const courseScheduleDB2 = await getCoursesSchedule();
 
 //*Clases INDIVIDUALES
-const individualCourseForm = document.getElementById('individualCourse');
-individualCourseForm.addEventListener('submit', async (e) => {
-	e.preventDefault();
-	try {
-		const priceInd = {
-			priceInd: Number(individualCourseForm['individual'].value),
-		};
-		await setDoc(doc(db, 'prices', 'individualPrice'), priceInd);
-		return priceInd;
-	} catch (err) {
-		throw new Error('clases individuales', err);
-	}
+const individualCourseForm = document.getElementById("individualCourse");
+individualCourseForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const priceInd = {
+      priceInd: Number(individualCourseForm["individual"].value),
+    };
+    await setDoc(doc(db, "prices", "individualPrice"), priceInd);
+    return priceInd;
+  } catch (err) {
+    throw new Error("clases individuales", err);
+  }
 });
 
 const priceIndDB = async () => {
-	try {
-		const newIndPriceDB = doc(db, 'prices', 'individualPrice');
-		const newIndPriceSnap = await getDoc(newIndPriceDB);
-		const newPriceDB = newIndPriceSnap.data();
-		return newPriceDB;
-	} catch (err) {
-		throw new Error('get individual price', err);
-	}
+  try {
+    const newIndPriceDB = doc(db, "prices", "individualPrice");
+    const newIndPriceSnap = await getDoc(newIndPriceDB);
+    const newPriceDB = newIndPriceSnap.data();
+    return newPriceDB;
+  } catch (err) {
+    throw new Error("get individual price", err);
+  }
 };
 
 export const newPriceIndDB = await priceIndDB();
 console.log(newPriceIndDB);
+
+//*CAMBRIDGE
+//*test
+const cambScheduleDataDB2 = { cambSchedule: [] };
+const cambForm = document.getElementById("cambScheduleDB");
+const addCambButton = document.getElementById("addCamb");
+addCambButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const course = {
+    name: cambForm["name"].value,
+    date: cambForm["date"].value,
+    day: cambForm["day"].value,
+    time: cambForm["time"].value,
+  };
+  cambScheduleDataDB2.cambSchedule.push(course);
+  console.log(cambScheduleDataDB2);
+  const showcamb = document.createElement("span");
+  cambForm.insertAdjacentElement("afterend", showcamb);
+  cambScheduleDataDB2.cambSchedule.forEach((course) => {
+    showcamb.innerHTML = `
+  ${course.name} | ${course.date} | ${course.day} | ${course.time}
+    `;
+    console.log(course);
+  });
+});
+//*publicar cursos preparacion examenes
+const publishSchedButtonCamb = document.getElementById(
+  "publishSchedButtonCamb"
+);
+publishSchedButtonCamb.addEventListener("click", async (e) => {
+  e.preventDefault();
+  try {
+    await setDoc(
+      doc(db, "coursesSchedule", "coursesScheduleCamb"),
+      cambScheduleDataDB2
+    );
+    await sweetalert2.fire({
+      title: " Cursos Cambridge PUBLICADOS!",
+      background: "#93E9BE",
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      width: "fit-content",
+      timer: 3000,
+      timerProgressBar: true,
+    });
+  } catch (err) {
+    sweetalert2.fire({
+      title: "ERROR!",
+      background: " #DB231C",
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      width: "fit-content",
+      timer: 3000,
+      timerProgressBar: true,
+    });
+    throw new Error("publish camb courses", err);
+  }
+});
+
+async function getCambSchedule() {
+  try {
+    const courseSshedulesCol = doc(
+      db,
+      "coursesSchedule",
+      "coursesScheduleCamb"
+    );
+    const courseSshedulesSnapshot = await getDoc(courseSshedulesCol);
+    const courseSshedulesList = courseSshedulesSnapshot.data();
+    return courseSshedulesList;
+  } catch (err) {
+    throw new Error("get CoursesShedule", err);
+  }
+}
+
+export const cambScheduleDB2 = await getCambSchedule();
